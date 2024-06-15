453100ef38672f5e418de00761d8a4179516e815
Samarth <142962370+samartho4@users.noreply.github.com>
2024-06-14 22:10:00 -0400

    Add files via upload

---
+++ Seneca-Library-Application-main/Utils.cpp
@@ -0,0 +1,28 @@
+/* Citation and Sources...
+Final Project Milestone 5
+Module: Utils.cpp
+Filename: Utils.cpp
+Version 1.0
+Author: Fardad Soleimanloo
+Revision History
+-----------------------------------------------------------
+Date      Reason
+2023/12/08  Preliminary release
+2023/02/08  Debugged DMA
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------*/
+
+
+#include <iostream> 
+#include "Utils.h"
+
+namespace sdds
+{
+    void thankYouDisplay()
+    {
+        std::cout << "-------------------------------------------\n";
+        std::cout << "Thanks for using Seneca Library Application\n";
+    }
+}
---
+++ Seneca-Library-Application-main/Utils.h
@@ -0,0 +1,26 @@
+/* Citation and Sources...
+Final Project Milestone 5
+Module: Utils.h
+Filename: utils.h
+Version 1.0
+Author: Fardad Soleimanloo
+Revision History
+-----------------------------------------------------------
+Date      Reason
+2023/12/08  Preliminary release
+2023/02/08  Debugged DMA
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------*/
+
+
+#ifndef SDDS_UTILS_H
+#define SDDS_UTILS_H
+
+namespace sdds
+{
+    void thankYouDisplay();
+}
+
+#endif
