e402f98721f346a8fc11db59de16245ab4b695d0
Samarth <142962370+samartho4@users.noreply.github.com>
2024-10-26 10:35:00 -0400

    Update Menu.cpp

--- Seneca-Library-Application-main/Menu.cpp
+++ Seneca-Library-Application-main/Menu.cpp
@@ -4,16 +4,6 @@ Module: Menu.cpp
 Filename: Menu.cpp
 Version 1.0
 Author: Fardad Soleimanloo
-Revision History
------------------------------------------------------------
-Date      Reason
-2023/21/07  Preliminary release
-2023/22/07  Debugged DMA
------------------------------------------------------------
-I have done all the coding by myself and only copied the code
-that my professor provided to complete my workshops and assignments.
------------------------------------------------------------*/
-
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
